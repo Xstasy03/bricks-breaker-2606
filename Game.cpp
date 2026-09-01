44eee9ff466fdf22398613bf5198e26aa7f4c956
Xstasy03 <Cmedinajoel@hotmail.com>
2026-09-01 16:58:54 -0400

    Steps 3 and 4

--- Game.cpp
+++ Game.cpp
@@ -30,8 +30,7 @@ void Game::Reset()
 	brick.doubleThick = true;
 	brick.color = ConsoleColor::DarkGreen;
 
-	for (int i = 0; i < 5; i++)
-	{
+	for (int i = 0; i < 5; i++) {
 		brick.x_position = i * brick.width;
 		bricks.push_back(brick);
 	}
@@ -78,6 +77,7 @@ void Game::Render() const
 	ball.Draw();
 
 	// TODO #3 - Update render to render all bricks
+	for (auto const& brick : bricks)
 	brick.Draw();
 
 	Console::Lock(false);
@@ -86,10 +86,12 @@ void Game::Render() const
 void Game::CheckCollision()
 {
 	// TODO #4 - Update collision to check all bricks
-	if (brick.Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
-	{
-		brick.color = ConsoleColor(brick.color - 1);
-		ball.y_velocity *= -1;
+	for (auto it = bricks.begin(); it != bricks.end(); ++it) {
+		if (it->Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity)) {
+
+			it->color = ConsoleColor(it->color - 1);
+			ball.y_velocity *= -1;
+		}
 
 		// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
 
